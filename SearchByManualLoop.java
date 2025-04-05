eb6d66eac686797568ef0a27c376e6ca1c86fa1b
Simeon Widdis <sawiddis@gmail.com>
2025-04-05 11:49:45 -0700

    Manual loop: use char and foreach instead of codepoints

--- SearchByManualLoop.java
+++ SearchByManualLoop.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 public class SearchByManualLoop {
 
-        static final int cpQuote = "\"".codePointAt(0);
-        static final int cpComma = ",".codePointAt(0);
-        static final int cpCr = "\r".codePointAt(0);
-        static final int cpNl = "\n".codePointAt(0);
+        static final char cpQuote = '\"';
+        static final char cpComma = ',';
+        static final char cpCr = '\r';
+        static final char cpNl = '\n';
 
         public static int count(List<String> data) {
                 int count = 0;
                 for (String line : data) {
-                        for (int i = 0; i < line.length(); i++) {
-                                int c = line.codePointAt(i);
+                        for (char c : line.toCharArray()) {
                                 if (c == cpQuote || c == cpComma || c == cpCr || c == cpNl) {
                                         count++;
                                         break;
